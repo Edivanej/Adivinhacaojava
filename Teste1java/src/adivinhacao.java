399a80911f6dba90baa090410888e4715bfd30ac
Edivane <edivanejunior1234@gmail.com>
2026-05-16 13:53:49 -0300

    commit

---
+++ Teste1java/src/adivinhacao.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class adivinhacao {
+    public static void main(String[] args) {
+        System.out.println("Jogo de adivinhação:\n");
+
+        int numeroSecreto =new Random().nextInt(100);
+
+        Scanner leitura = new Scanner(System.in);
+
+        int tentativas = 0;
+
+        while (tentativas <= 25) {
+            System.out.println("Qual numero você acha que é:");
+
+            int chute = leitura.nextInt();
+            tentativas ++;
+
+            if(chute <= numeroSecreto){
+                System.out.println("É menor");
+            } if (chute >= numeroSecreto) {
+                System.out.println("É maior");
+            } if (chute == numeroSecreto){
+                System.out.println("Acertou");
+                break;
+            }
+            if (tentativas == 25) {
+                System.out.println("Suas tentativas acabaram! O número era: " + numeroSecreto);
+            }
+        }
+
+
+
+    }
+}
